3df566220c710b122a46004dedf53877ca37983d
AlexU93 <yuvshin.alex@yandex.ru>
2022-03-08 17:14:52 +0300

    "hw2_TaskFour"

---
+++ src/hw2/TaskFour.java
@@ -0,0 +1,18 @@
+package hw2;
+
+public class TaskFour {
+    public static void main(String[] args) {
+        int age = 99;
+        int exp = 6;
+        if (age>100) {
+            System.out.println("Мы вам перезвоним");
+        }
+        else if (exp<5) {
+            System.out.println("Вы подходите на должность стажера");
+        }
+        else {
+            System.out.println("Вы подходите на должность разработчика");
+        }
+        }
+    }
+
