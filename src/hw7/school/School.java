bf89a5ae96cedeb7d326d4913a8ef2f8b7f3bcf7
AlexU93 <yuvshin.alex@yandex.ru>
2022-03-24 01:21:41 +0300

    "1"

--- src/hw7/school/School.java
+++ src/hw7/school/School.java
@@ -3,22 +3,32 @@ package hw7.school;
 import hw7.headmaster.HeadMaster;
 import hw7.student.Student;
 import hw7.teacher.Teacher;
+import hw7.base.skillSubject.SkillSubject;
 
 public class School {
 
 
     private String schoolName;
     private HeadMaster headMaster;
-    private Teacher [] teachers = new Teacher[4];
-    private Student [] students = new Student[4];
+    private Teacher[] teachers = new Teacher[4];
+    private Student[] students = new Student[4];
 
     public School(HeadMaster headMaster, String schoolName) {
         this.headMaster = headMaster;
+        this.schoolName = schoolName;
     }
 
-    private void setSchoolName (String schoolName) {
-        if (schoolName == null)
+    public static Teacher getTeacher(Teacher teacher) {
+        if (teacher.getSubject().equalsIgnoreCase("математика")) {
+
+        }
     }
 
-
+    public static Teacher[] getTeachers(int count) {
+        Teacher[] teachers = new Teacher[count];
+        for (int i = 0; i < teachers.length; i++) {
+            teachers[i] = getTeacher();
+        }
+        return teachers;
+    }
 }
--- src/hw7/teacher/Teacher.java
+++ src/hw7/teacher/Teacher.java
@@ -6,20 +6,22 @@ import hw7.student.Student;
 
 public class Teacher extends SkillSubject {
     protected int teacherSkill = 100;
-    protected String subjectName;
 
 
     public Teacher(String name, int age, String subject) {
         super(name, age, subject);
     }
 
-    public void teacherNameSubject () {
+    public void teacherNameSubject() {
         System.out.println("Teacher name" + "-" + name + " " + "teach" + "-" + subject);
     }
 
-    public void teach (Student student) {
+
+
+    public void teach(Student student) {
         student.beTeacher(teacherSkill);
     }
+
 
 }
 
